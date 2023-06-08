374ff865840a6a6d1bbde71a09683bb95928016a
Driss Guessous <drisspg@fb.com>
2023-06-08 22:46:47 +0000

    benchmark utils

---
+++ transformer_nuggets/utils/benchmark.py
@@ -0,0 +1,48 @@
+import torch
+import torch.utils.benchmark as benchmark
+from pathlib import Path
+import random
+from torch.profiler import profile, record_function, ProfilerActivity
+from dataclasses import dataclass
+from typing import Optional, Callable
+from contextlib import nullcontext
+
+
+@dataclass
+class ProfileConfig:
+    file_path: Optional[Path] = None
+    name: Optional[str] = None
+    cuda: bool = True
+    iters: int = 0
+    warmup_iters: int = 0
+
+
+def benchmark_torch_function_in_microseconds(func: Callable, *args, **kwargs) -> float:
+    t0 = benchmark.Timer(
+        stmt="func(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "func": func}
+    )
+    return t0.blocked_autorange().mean * 1e6
+
+
+def profile_function(config: ProfileConfig, func: Callable, *args, **kwargs) -> None:
+    """Profile a torch function and save the result to a file"""
+    seed = 123
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    activities = [ProfilerActivity.CPU]
+    if config.cuda:
+        activities.append(ProfilerActivity.CUDA)
+
+    if config.warmup_iters >= 0:
+        for _ in range(config.warmup_iters):
+            func(*args, **kwargs)
+
+    name_context = nullcontext() if config.name is None else record_function(config.name)
+    with profile(activities=activities, record_shapes=False) as prof:
+        for _ in range(config.iters):
+            with name_context:
+                func(*args, **kwargs)
+
+    if config.file_path is not None:
+        prof.export_chrome_trace(config.file_path)
