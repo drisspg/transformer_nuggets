e0054b93d5110f9bc56c05b6f3d120ba17e94afe
drisspg <drisspguessous@gmail.com>
2023-11-05 18:17:00 -0800

    saturated option

--- test/test_fp8.py
+++ test/test_fp8.py
@@ -4,6 +4,16 @@ import torch
 from transformer_nuggets.fp8.scaled_quant import scaled_quant
 
 
+def eager_scaled_quant(a, scale, abs_max, fp8_dtype, saturated=False):
+    out = a * scale
+    if saturated:
+        out = torch.where(out > torch.finfo(fp8_dtype).max, torch.finfo(fp8_dtype).max, out)
+        out = torch.where(
+            out < -1 * torch.finfo(fp8_dtype).max, -1 * torch.finfo(fp8_dtype).max, out
+        )
+    return out.to(fp8_dtype), torch.max(torch.abs(out))
+
+
 @pytest.mark.parametrize("fp8_dtype", [torch.float8_e4m3fn, torch.float8_e5m2])
 def test_basic_quant(fp8_dtype):
     torch.manual_seed(0)
@@ -14,6 +24,23 @@ def test_basic_quant(fp8_dtype):
     output = scaled_quant(a, scale, abs_max, fp8_dtype)
     torch.testing.assert_close(output, (a * scale).to(fp8_dtype))
     torch.testing.assert_close(abs_max, torch.tensor([9.6], dtype=torch.float32, device="cuda"))
+
+
+@pytest.mark.parametrize("fp8_dtype", [torch.float8_e4m3fn, torch.float8_e5m2])
+def test_saturated(fp8_dtype):
+    torch.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+    a = torch.rand(2**12, 2**12, dtype=torch.float32, device="cuda")
+    a[0, :100] = torch.finfo(fp8_dtype).max + 100
+    scale = torch.tensor([4.0], dtype=torch.float32, device="cuda")
+    abs_max = torch.tensor([-1.0], dtype=torch.float32, device="cuda")
+    output = scaled_quant(a, scale, abs_max, fp8_dtype, saturated=True)
+    eager_output, eager_abs_max = eager_scaled_quant(a, scale, abs_max, fp8_dtype, saturated=True)
+    torch.testing.assert_close(output, eager_output)
+    torch.testing.assert_close(
+        abs_max,
+        torch.tensor([torch.finfo(fp8_dtype).max + 100], dtype=torch.float32, device="cuda"),
+    )
 
 
 if __name__ == "__main__":
--- transformer_nuggets/fp8/scaled_quant.py
+++ transformer_nuggets/fp8/scaled_quant.py
@@ -12,6 +12,7 @@ def scaled_cast(
     numel: int,
     XBLOCK: tl.constexpr,
     float8_dtype: tl.constexpr,
+    max_val: tl.constexpr,
 ):
     """Quantize tensor to fp8 using a delayed scaled and calculate abs_max"""
     offset = tl.program_id(0) * XBLOCK
@@ -22,6 +23,11 @@ def scaled_cast(
     tl.atomic_max(abs_max_ptr, block_max)
     scale = tl.load(scale_ptr)
     scaled_inpt = inpt * scale
+    if max_val != 0.0:
+        # Wanted to pass in a `saturated : bool` but it doesn't work
+        # and can't branch off of tl.dtype
+        tl.where(scaled_inpt > max_val, max_val, scaled_inpt)
+        tl.where(scaled_inpt < -1 * max_val, -1 * max_val, scaled_inpt)
     tl.store(output_ptr + (index), scaled_inpt.to(float8_dtype), mask=mask)
 
 
@@ -30,16 +36,31 @@ def scaled_quant(
     scale: torch.Tensor,
     abs_max: torch.Tensor,
     fp8_dtype: torch.dtype = torch.float8_e4m3fn,
+    saturated: bool = False,
 ):
-    """Quantize tensor to fp8 using a delayed scaled and calculate abs_max"""
+    """Quantize tensor to fp8 using a delayed scaled and calculate abs_max
+    for use in the next iteration of quantization.
+
+    Args:
+        inpt_tensor: Input tensor to quantize
+        scale: Scale to apply to input tensor, calculated from previous abs_max
+        abs_max: Absolute maximum value of input tensor, will be updated
+        fp8_dtype: FP8 datatype to quantize to
+        saturated: Whether to saturate the output tensor to the maximum value
+            of the fp8 datatype
+    """
     assert scale.dtype == torch.float32
     assert abs_max.dtype == torch.float32
     assert scale.numel() == 1
     assert abs_max.numel() == 1
+    assert inpt_tensor.is_contiguous(), "Input tensor must be contiguous"
 
     out_tensor = torch.empty_like(inpt_tensor, dtype=fp8_dtype, device="cuda")
     numel = inpt_tensor.numel()
     grid = lambda meta: (triton.cdiv(numel, meta["XBLOCK"]),)
     tl_dtype = {torch.float8_e4m3fn: tl.float8e4nv, torch.float8_e5m2: tl.float8e5}[fp8_dtype]
-    scaled_cast[grid](inpt_tensor, out_tensor, scale, abs_max, numel, 4096, tl_dtype, num_warps=8)
+    max_val = torch.finfo(fp8_dtype).max if saturated else 0.0
+    scaled_cast[grid](
+        inpt_tensor, out_tensor, scale, abs_max, numel, 4096, tl_dtype, max_val, num_warps=8
+    )
     return out_tensor
