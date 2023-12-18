5c89445a64a3d259b14e52c0d2ee334d9fb34a30
Driss Guessous <32754868+drisspg@users.noreply.github.com>
2023-12-17 18:05:27 -0800

    Add benchmark fp8 script (#9)
    * working
    
    * add torch_compile reference
    
    * compile is annoying to microbenchmark, skipping for now

--- transformer_nuggets/fp8/scaled_quant.py
+++ transformer_nuggets/fp8/scaled_quant.py
@@ -17,6 +17,7 @@ def scaled_cast(
     """Quantize tensor to fp8 using a delayed scaled and calculate abs_max"""
     offset = tl.program_id(0) * XBLOCK
     index = offset + tl.arange(0, XBLOCK)[:]
+    index = tl.max_contiguous(tl.multiple_of(index, XBLOCK), XBLOCK)
     mask = index < numel
     inpt = tl.load(inpt_ptr + (index), mask=mask)
     block_max = tl.max(tl.abs(inpt))
@@ -64,3 +65,30 @@ def scaled_quant(
         inpt_tensor, out_tensor, scale, abs_max, numel, 4096, tl_dtype, max_val, num_warps=8
     )
     return out_tensor
+
+
+def eager_scaled_quant(
+    a: torch.Tensor,
+    scale: torch.Tensor,
+    abs_max: torch.Tensor,
+    fp8_dtype: torch.dtype,
+    saturated: torch.dtype = False,
+):
+    """Quantize tensor to fp8 using a delayed scaled and calculate abs_max
+
+    Args:
+        a: Input tensor to quantize
+        scale: Scale to apply to input tensor, calculated from previous abs_max
+        abs_max: Absolute maximum value of input tensor, will be updated
+        fp8_dtype: FP8 datatype to quantize to
+        saturated: Whether to saturate the output tensor to the maximum value
+            of the fp8 datatype
+    """
+    out = a * scale
+    if saturated:
+        out = torch.where(out > torch.finfo(fp8_dtype).max, torch.finfo(fp8_dtype).max, out)
+        out = torch.where(
+            out < -1 * torch.finfo(fp8_dtype).max, -1 * torch.finfo(fp8_dtype).max, out
+        )
+    abs_max = torch.max(torch.abs(out))
+    return out.to(fp8_dtype)
