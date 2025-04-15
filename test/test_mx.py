d5a3472c3556526d15fb42f8e645f6ef03a80b97
drisspg <drisspguessous@gmail.com>
2025-04-15 09:20:41 -0700

    update
    stack-info: PR: https://github.com/drisspg/transformer_nuggets/pull/47, branch: drisspg/stack/8

--- test/test_mx.py
+++ test/test_mx.py
@@ -9,6 +9,7 @@ from transformer_nuggets.mx.to_blocked import (
 )
 
 
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
 @pytest.mark.parametrize("device", ["cpu", "cuda"] if torch.cuda.is_available() else ["cpu"])
 def test_individual(device):
     scales = torch.randint(256, size=(128, 4), device="cuda", dtype=torch.uint8)
@@ -17,6 +18,7 @@ def test_individual(device):
     assert torch.equal(single, single_vmap)
 
 
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
 @pytest.mark.parametrize("shape", [(128, 4), (256, 8), (300, 9)])
 def test_rearrange(shape):
     scales = torch.randint(256, size=shape, device="cuda", dtype=torch.uint8)
